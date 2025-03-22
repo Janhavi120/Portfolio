2c342cd7fe9d7c6dc33007c4fbb1fd5fb68e5cc7
Janhavi120 <152052921+Janhavi120@users.noreply.github.com>
2025-03-22 11:43:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const typed = new Typed(".multiple-text", {
+        strings: ["Python Developer", "Frontend Developer", "Backend Developer", "Web Designer"],
+        typeSpeed: 90,
+        backSpeed: 90,
+        backDelay: 1200,
+        loop: true
+    });
+});
