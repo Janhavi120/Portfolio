95efc7025db9f7a89410b79ab7c62fd8479bd8d0
Janhavi120 <152052921+Janhavi120@users.noreply.github.com>
2025-03-22 14:12:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const typed = new Typed(".multiple-text", {
-        strings: ["Python Developer", "Frontend Developer", "Backend Developer", "Web Designer"],
-        typeSpeed: 90,
-        backSpeed: 90,
-        backDelay: 1200,
-        loop: true
-    });
-});
